af85d8b9db34cc1fd8ff4cf3b2345eb698dbb7bc
Oliver Brown <olliebrown3@hotmail.co.uk>
2022-03-03 16:33:43 +0000

    added animation app, solved rendering issue of static css file, seemed to be about re-starting runserver to get static files configured

--- ui_practice/urls.py
+++ ui_practice/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('single_page/', include("single_page.urls")),
-    path('infinite_scroll/', include("infinite_scroll.urls"))
+    path('infinite_scroll/', include("infinite_scroll.urls")),
+    path('animation/', include("animation.urls"))
 ]
