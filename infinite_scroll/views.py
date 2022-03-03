ad56e8724f5b25faf92853c6ab17f2ce6ed4b941
Oliver Brown <olliebrown3@hotmail.co.uk>
2022-03-03 11:51:00 +0000

    add infinite scroll app, fixed path of .js src to get working

--- infinite_scroll/views.py
+++ infinite_scroll/views.py
@@ -1,14 +1,15 @@
 import time
+
 from django.http import JsonResponse
 from django.shortcuts import render
 
 # Create your views here.
-
 def index(request):
     return render(request, "infinite_scroll/index.html")
 
 def posts(request):
-     # Get start and end points from url parameters
+
+    # Get start and end points
     start = int(request.GET.get("start") or 0)
     end = int(request.GET.get("end") or (start + 9))
 
